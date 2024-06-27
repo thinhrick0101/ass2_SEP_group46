2d5d0d8dc08482c4d0dda02b3edb8e3e01ba4a93
thinhrick0101 <122353285+thinhrick0101@users.noreply.github.com>
2024-06-28 02:15:57 +0700

    Update test_calculator.py

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,27 +1,27 @@
-  """
+"""
 This is a comment
 """
 import unittest
 from calculator import divide
 class TestDivision(unittest.TestCase):
     def test_divide_normal(self):
-         """
-This is a comment
-"""
+        """
+        This is a comment
+        """
         self.assertAlmostEqual(divide(10, 2), 5.0, places=4, msg="10 / 2 should be 5")
         self.assertAlmostEqual(divide(9, 3), 3.0, places=4, msg="9 / 3 should be 3")
         
     def test_divide_with_decimals(self):
-         """
-This is a comment
-"""
+        """
+        This is a comment
+        """
         self.assertAlmostEqual(divide(5, 2), 2.5, places=4, msg="5 / 2 should be 2.5")
         self.assertAlmostEqual(divide(7, 3), 2.3333, places=4, msg="7 / 3 should be approximately 2.3333")
 
     def test_divide_by_zero(self):
         """
-This is a comment
-"""
+        This is a comment
+        """
         with self.assertRaises(ValueError) as context:
             divide(1, 0)
         self.assertEqual(str(context.exception), "Cannot divide by zero")
